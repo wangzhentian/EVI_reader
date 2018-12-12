993cb400320fb66c4b8d9c088091103fff922212
Zhentian Wang <wangzhentian@zhentian.psi.ch>
2018-12-12 16:31:19 +0100

    add test script

---
+++ test.py
@@ -0,0 +1,11 @@
+import EVIFile as EVI
+from EVIFile import EVIread
+
+filename = '/Users/wangzhentian/Desktop/test1_TE.EVI'
+fEVI = EVI.EVIFile(filename)
+#fEVI.read(filename)
+fEVI.shape()
+#fEVI.print_header()
+#fEVI.show()
+
+data, headers = EVIread(filename)
